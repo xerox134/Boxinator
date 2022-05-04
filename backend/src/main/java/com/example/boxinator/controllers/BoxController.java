88924429a0106a1d6426f65709935431ab6626c7
Stefan <Stefan_mijatovic97@hotmail.com>
2022-05-04 23:23:29 +0200

    Fetch to frontend

--- backend/src/main/java/com/example/boxinator/controllers/BoxController.java
+++ backend/src/main/java/com/example/boxinator/controllers/BoxController.java
@@ -3,6 +3,7 @@ package com.example.boxinator.controllers;
 import com.example.boxinator.entities.Box;
 import com.example.boxinator.services.BoxService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class BoxController {
     }
 
     @GetMapping("/rest/boxes")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Box> getAllBoxes(){
         return boxService.getAll();
     }
--- frontend/src/Components/ItemDetail.js
+++ frontend/src/Components/ItemDetail.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import '../App.css'
 
 export default function Item({match}) {
 
@@ -17,24 +18,49 @@ export default function Item({match}) {
 //     console.log(items.data);
 //  }
 
-const [boxes, setBoxes] = useState([{}]);
+const [boxes, setBoxes] = useState([]);
 
   async function fetchMyThreads() {
-    const raw = await fetch(`/rest/boxes`);
-    const res = await raw.json();
+    const raw = await fetch(`http://localhost:8080/rest/boxes`);
+    console.log("raw", raw)
 
+    const res = await raw.json();
+    console.log("raw", raw)
+    console.log("res", res)
     setBoxes(res)
   }
 
+  const getBoxes = async () => {
+
+    let response = await fetch("http://localhost:8080/rest/boxes");
+      response = await response.json();
+      response.forEach((element) => {
+      
+        setBoxes(element);
+        })}
+
   useEffect(() => {
-    fetchMyThreads();
+    // fetchMyThreads();
+    getBoxes()
   }, []);
 
   return (
     <div>
 
 <h1>Item</h1>
-<h3>{boxes}</h3>
+
+<div className='container'>
+<div className='sub-container'>
+<div><h3>Name</h3></div>
+<div><h3>Country</h3></div>
+<div><h3>Weight</h3></div>
+</div>
+<div className='sub-container'>
+<div>{boxes.name}</div>
+<div>{boxes.country}</div>
+<div>{boxes.weight}</div>
+</div>
+</div>
     </div>
 
   )
