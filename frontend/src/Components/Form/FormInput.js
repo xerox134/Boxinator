0d6c1399350c4d018fe4bad66e06b5acc916423a
Stefan <Stefan_mijatovic97@hotmail.com>
2022-05-08 18:13:20 +0200

    newForm 0.2

--- frontend/src/Components/Form/FormInput.js
+++ frontend/src/Components/Form/FormInput.js
@@ -1,16 +1,64 @@
-import React from 'react';
+//////////
+import React, { useState } from 'react';
+
 import "./FormInput.css";
+import { PhotoshopPicker } from "react-color";
 
 export default function FormInput(props) {
   const { label, onChange, errorMessage, id, ...inputProps } = props;
 
+  const [color, setColor] = useState();
 
+    const handleOnChange=(e,m)=>{
+       
 
-  return <div className="formInput">
-       <label>{label}</label>
+console.log("inside",e)
+console.log("inside",m)
+onChange(m)
+    }
+
+  const renderForms = () => {
+    if (id === 1) {
+      return (
+        <div>
+          <label>{label}</label>
           <input {...inputProps} onChange={onChange} />
           <span>{errorMessage}</span>
-          </div>;
+        </div>
+      );
+    } else if (id === 2) {
+      return (
+        <div>
+          <label>{label}</label>
+          <input {...inputProps} onChange={onChange} />
+          <span>{errorMessage}</span>
+        </div>
+      );
+    } else if (id === 3) {
+      return (
+        <div>
+          <label>{label}</label>
+          <div>
+          <PhotoshopPicker color={color} onChange={handleOnChange} /></div>
+          <span>{errorMessage}</span>
+        </div>
+      );
+    } else if (id === 4) {
+      return (
+        <div>
+          <label>{label}</label>
+          <select {...inputProps} onChange={onChange}>
+            <option value="Sweden">Sweden</option>
+            <option value="China">China</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Australia">Australia</option>
+          </select>
+          <span>{errorMessage}</span>
+        </div>
+      );
+    } else {
+    }
+  };
+
+  return <div className="formInput">{renderForms()}</div>;
 }
-
-
